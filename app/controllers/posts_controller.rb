492048a105df292566ca3ca7f92d2a3243e8f872
zair Vamp <zair.100.pro@gmail.com>
2019-07-08 20:27:54 +0500

    all except delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
     def index
+        @posts = Post.all.order("created_at DESC")
     end
     
     def show
@@ -16,6 +17,10 @@ class PostsController < ApplicationController
     def update
         @post = Post.find(params[:id])
         if @post.update(post_params)
+            redirect_to @post
+        else
+            render "edit"
+        end
     end
 
     def create
