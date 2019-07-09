962ab237ddff0a8acfb13daee2dc11fc9aef3910
zair Vamp <zair.100.pro@gmail.com>
2019-07-09 20:09:53 +0500

    phase one is complete basic crud with design

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,6 +33,14 @@ class PostsController < ApplicationController
             render "new"
         end
     end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+
+        redirect_to posts_path
+    end
+
 private
     def post_params
         params.require(:post).permit(:title,:body)
